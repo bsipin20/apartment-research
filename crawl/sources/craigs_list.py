5bef3769eecbcfe6947a189a25a39c3640ac3314
brian <bsipin@gmail.com>
2022-01-17 20:16:14 -0500

    adding more identifiers

--- crawl/sources/craigs_list.py
+++ crawl/sources/craigs_list.py
@@ -13,23 +13,26 @@ from crawl import helpers
 Coordinate = namedtuple('Coordinate', ['x','y'])
 
 def search_craigslist_housing(site: str, area, category: str = 'apa'):
+
+        yield result
+
+def main(site = "newyork",
+         area = "brk",
+         category = 'apa',
+         num_postings = 5):
+
+    count = 0
     cl_h = CraigslistHousing(site=site, area = area,
-                             category='apa',
+                             category=category,
                              filters={'min_price': 1000,
                                       'max_price': 4000,
                                       'min_bedrooms': 2,
                                       'max_bedrooms': 2})
 
+
     for result in cl_h.get_results(sort_by='newest', geotagged=True):
-        yield result
 
-def main(site = "newyork",
-         area = "brk",
-         num_postings = 5):
-
-    count = 0
-    for result in search_craigslist_housing("newyork", "brk"):
-        if count > 5:
+        if count >  num_postings:
             sys.exit()
         else:
             price = helpers.parse_price(result['price'])
@@ -38,13 +41,17 @@ def main(site = "newyork",
             datetime = result['datetime'].split(" ")[0]
             location = helpers.parse_where(result['where'].lower())
             url = result['url']
-            id_ = int(result['id'])
+            id_ = "craigslist_" + result['id']
 
-            msg = helpers.create_message("postings", symbols = { 'url': url,
+            msg = helpers.create_message("postings", symbols = {  'gid': id_,
+                                                                  'site': site,
+                                                                  'category': category,
+                                                                  'area' : area,
                                                                   'last_updated': last_updated,
                                                                   'date': datetime,
-                                                                  'place': location},
-                                                      fields= {'posting_id': id_, 'lon': lon, 'lat': lat,'price': int(price)})
+                                                                  'place': location,
+                                                                  'url' : url},
+                                                      fields= {'lon': lon, 'lat': lat,'price': int(price)})
 
             print(msg)
             helpers.send_tcp_messages(msg)
